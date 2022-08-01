8708a2485a6ceacfa8053f5b7388f85f1ee67910
Aaron TABUCHI <aarontabuchi@aarons-mbp.lan>
2022-08-01 14:42:56 -0500

    dif fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ path = Tk()
 
 path.title("MeowEngine! 🐱")
 
-canvas = Canvas(path, width=1000, height=1000, color="#333332")
+canvas = Canvas(path, width=1000, height=1000, background="#333332")
 
 canvas.pack()
 path.mainloop()
