a46d7e41022468f6719d311e44e632b5aac3b3a6
Aaron TABUCHI <aarontabuchi@aarons-mbp.lan>
2022-08-01 17:17:51 -0500

    trying to fix

--- MeowEngine.py
+++ MeowEngine.py
@@ -1,22 +1,18 @@
 from tkinter import *
 
 class MeowEngine:
-    def __init__(self):
-
+    def __init__(self, isEnabled):
+        self.enabled = isEnabled
         self.path = Tk()
 
         self.path.title("MeowEngine! 🐱")
 
         self.canvas = Canvas(path, width=1000, height=1000, background="#333332")
 
-
-
         self.canvas.pack()
         self.path.mainloop()
 
-    def __init__(self,isEnabled):
-            self.enabled = isEnabled
-        
+    
     def drawRect(x,y,w,h,outline,fill):
-        if isEnabled:
-            canvas.create_rectangle(x, y, w, h,outline, fill)
+        if self.isEnabled:
+            self.canvas.create_rectangle(x, y, w, h,outline, fill)
