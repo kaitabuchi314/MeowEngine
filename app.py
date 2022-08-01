2595168dff9881ced8b63f342c8921c1208d41db
Aaron TABUCHI <aarontabuchi@aarons-mbp.lan>
2022-08-01 14:39:53 -0500

    fix broken colors

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from tkinter import *
-from turtle import bgcolor
 
 path = Tk()
 
 path.title("MeowEngine! 🐱")
 
-canvas = Canvas(path, width=1000, height=1000, bgcolor="#333332")
+canvas = Canvas(path, width=1000, height=1000, color="#333332")
 
 canvas.pack()
 path.mainloop()
