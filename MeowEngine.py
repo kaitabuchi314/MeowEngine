9f4f13296b8f1eeba7b4c7d311eeed85bc78f35e
Aaron TABUCHI <aarontabuchi@aarons-mbp.lan>
2022-08-01 19:26:29 -0500

    trying to fix render bug

--- MeowEngine.py
+++ MeowEngine.py
@@ -11,6 +11,7 @@ class MeowEngine:
         self.canvas.pack()
         self.path.mainloop()
 
-    
+    def drawLine(self, x1,y1,x2,y2):
+        self.canvas.create_line(15, 25, 200, 25)
     def drawRect(self, x,y,w,h,outline,fill):
         self.canvas.create_rectangle(x, y, x+w, y+h,outline=outline, fill=fill)
